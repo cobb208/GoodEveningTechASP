d414e631a295dddcc74a68c19d01e02e95fec572
corycobb <cory.m.cobb@outlook.com>
2022-06-23 20:01:43 -0400

    blockquotes

---
+++ MDEngine/Tags/BlockQuoteTag.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace MDEngine.Tags
+{
+	public class BlockQuoteTag
+	{
+		private bool _isBlockQuote = false;
+
+		public bool IsBlockQuote => _isBlockQuote;
+
+		public BlockQuoteTag()
+		{
+		}
+
+
+		public string Create()
+        {
+			if(!_isBlockQuote)
+            {
+				_isBlockQuote = true;
+				return "<blockquote>";
+            }
+
+			return "";
+        }
+
+		public string Close()
+        {
+			if (_isBlockQuote)
+			{
+				_isBlockQuote = false;
+				return "</blockquote>";
+			}
+			return "";
+        }
+	}
+}
+
